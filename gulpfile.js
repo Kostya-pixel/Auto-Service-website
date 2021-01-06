8d162638c3a7e570168358c9453011bad4ae3768
Konstantin <konstantinpavlovich1987@gmail.com>
2021-01-06 15:25:07 +0200

    start a new website

--- gulpfile.js
+++ gulpfile.js
@@ -36,9 +36,3 @@ gulp.task('watch', gulp.parallel('brSync', function() {
 gulp.task('start', gulp.parallel( 'html', 'js', 'sass','watch'));
 
 
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin https://github.com/Kostya-pixel/calendar.git
-// git push -u origin main
